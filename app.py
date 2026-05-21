24fdfc648e7115c34d4c5c3e9042784da09ee825
Sairam Parre <sairamnani23gmail@gmail.com>
2026-05-21 19:29:48 +1000

    Fix Flask template and clean repository

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask 
-app = Flask(__name__) 
-@app.route('/') 
-def home(): 
-    return "Dress Delivery Service - Order Tracking System" 
+from flask import Flask, render_template
 
-if __name__ == "__main__": 
-    app.run(debug=True) 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(debug=True)
