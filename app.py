062aba7d3cb0a030a458990bc6081e6f4af338df
Sairam Parre <sairamnani23gmail@gmail.com>
2026-05-21 19:23:59 +1000

    updated flask app

--- app.py
+++ app.py
@@ -1,1 +1,8 @@
-print("Dress Delivery Service started") 
+from flask import Flask 
+app = Flask(__name__) 
+@app.route('/') 
+def home(): 
+    return "Dress Delivery Service - Order Tracking System" 
+
+if __name__ == "__main__": 
+    app.run(debug=True) 
